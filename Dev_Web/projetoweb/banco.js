91fea4aa240e24be49c0e34c3cb3963f69887188
Lucas Franco <136494921+LucasCoFranco@users.noreply.github.com>
2024-05-24 08:20:36 -0300

    Update banco.js

--- Dev_Web/projetoweb/banco.js
+++ Dev_Web/projetoweb/banco.js
@@ -8,4 +8,31 @@ sequelize.authenticate().then(() => {
     console.log("Conectado com sucesso");
 }).catch((erro) => {
     console.error("Falha ao conectar: ", erro);
+}); //isso acima é a criação do banco de dados com o xampp
+
+sequelize.authenticate().then(() => {
+    console.log("Conectado com sucesso");
+}).catch((erro) => {
+    console.error("Falha ao conectar: ", erro);
 });
+
+const Agendamentos = sequelize.define('agendamentos',{
+    nome:{
+        type: Sequelize.STRING
+    },
+    endereco:{
+        type: Sequelize.STRING
+    },
+    bairro:{
+        type: Sequelize.STRING
+    },
+    cep: {
+        type: Sequelize.INTEGER
+    }
+})
+
+Agendamentos.sync({force: true}).then(() => {
+    console.log("Tabela criada com sucesso!")
+}).catch((error) => {
+    console.log("Erro ao criar tabela: " + error)
+}) //isso acima é a criação da tabela "agendamentos" no banco de dados que foi criado logo acima
